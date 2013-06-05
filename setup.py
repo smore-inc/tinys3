7fc28d751c88907a75b324681ef41e1078288060
Shlomi Atar <shlomiatar@gmail.com>
2013-06-04 23:47:01 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='tinys3',
-      version='0.1.6',
+      version='0.1.7',
       description=("A small library for uploading files to S3,"
                    "With support of async uploads, worker pools, cache headers etc"),
 
