7fd6aa53bdf245a992af5e3c61a37fb5bf2acc62
Shlomi Atar <shlomiatar@gmail.com>
2016-10-14 13:05:22 +0300

    Update __init__.py

--- tinys3/__init__.py
+++ tinys3/__init__.py
@@ -8,7 +8,7 @@ from .multipart_upload import MultipartUpload
 from .connection import Connection as Conn
 
 __title__ = 'tinys3'
-__version__ = '0.1.11'
+__version__ = '0.1.12'
 __author__ = 'Shlomi Atar'
 __license__ = 'Apache 2.0'
 __all__ = ["Connection", "Conn", "Pool", "MultipartUpload"]
